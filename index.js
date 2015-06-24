877f217739af58132b85900ab02bdf04d676246f
Sergey Chikuyonok <serge.che@gmail.com>
2015-06-24 20:01:45 +0300

    Working implementation of simple project import

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var stream = require('stream');
-var locateSymlinks = require('./lib/locate-symlinks');
-var symlinkConfig = require('./lib/symlink-config');
+var ReadConfigStream = require('./lib/stream/read-config');
+var RewriteUrlStream = require('./lib/stream/rewrite-url');
+var ExportProjectStream = require('./lib/stream/export-project');
 
 /**
  * Creates readable stream with project configs in given `src` path
@@ -29,63 +30,5 @@ module.exports.dest = function(dest) {
  * @return {RewriteUrlStream}
  */
 module.exports.rewriteUrl = function(config) {
-	
+	return new RewriteUrlStream(config);
 };
-
-class ReadConfigStream extends stream.Readable {
-	constructor(src) {
-		super({objectMode: true});
-		this.src = src;
-		this._reader = null;
-		this._symlinks = null;
-		this._process = this.processSymlink.bind(this);
-		this.once('end', this.onEnd);
-	}
-
-	_read() {
-		if (!this._reader) {
-			var self = this;
-			this._reader = locateSymlinks(this.src).then(function(symlinks) {
-				self._symlinks = symlinks.slice(0);
-			});
-		}
-
-		this._reader.then(self._process);
-	}
-
-	processSymlink() {
-		if (!this._symlinks || !this._symlinks.length) {
-			// finishing stream
-			return this.push(null);
-		}
-
-		var self = this;
-		symlinkConfig.create(this._symlinks.pop(), this._symlinks.dir)
-		.then(function(config) {
-			if (self.push(config)) {
-				self._process();
-			}
-		}, function(err) {
-			self.emit('error', err);
-		});
-	}
-
-	onEnd() {
-		this._reader = this._symlinks = this._process = null;
-	}
-};
-
-class ExportProjectStream extends stream.Writable {
-	constructor(dest) {
-		super({objectMode: true});
-		this.dest = dest;
-	}
-
-	_write(config, enc, next) {
-
-	}
-};
-
-class RewriteUrlStream extends stream.Transform {
-	
-}
