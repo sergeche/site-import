3269c9db20c7cfa0efcb5806e7dc88c6b72e82b6
Sergey Chikuyonok <serge.che@gmail.com>
2016-06-09 18:31:44 +0300

    Allow project info override

--- test/project-config.js
+++ test/project-config.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const assert = require('assert');
 const stream = require('stream');
 const vfs = require('vinyl-fs');
@@ -8,7 +9,7 @@ const dummy = require('./assets/dummy-writer');
 
 describe('Detect project config for file', () => {
     it('file-based config', done => {
-		var fileMap = {};
+		const fileMap = {};
 
 		vfs.src('./in/**', {cwd: __dirname, follow: true, nodir: true})
 		.pipe(projectConfig()).on('error', done)
@@ -35,6 +36,45 @@ describe('Detect project config for file', () => {
 			assert.equal(project.prefix, '/p2');
 			assert.equal(project.name, 'p2', 'Get project name from parent folder');
 			assert.equal(project.url, '/img/smiley.png');
+
+			done();
+		});
+	});
+
+	it('override config', done => {
+		const fileMap = {};
+		const prefix = '/custom/prefix';
+
+		vfs.src('./in/**', {cwd: __dirname, follow: true, nodir: true})
+		.pipe(projectConfig({
+			project: {
+				baseDir: path.resolve(__dirname, './in'),
+				prefix
+			}
+		})).on('error', done)
+		.pipe(new stream.Transform({
+			objectMode: true,
+			transform(file, enc, next) {
+				fileMap[file.relative] = file.project;
+				next(null, file);
+			}
+		}))
+		.pipe(dummy())
+		.on('end', () => {
+			let project = fileMap['build.sh'];
+			assert.equal(project.prefix, prefix);
+			assert.equal(project.name, 'in', 'Get project name from overridden baseDir folder');
+			assert.equal(project.url, '/build.sh');
+
+			project = fileMap['p1/about/index.html'];
+			assert.equal(project.prefix, prefix);
+			assert.equal(project.name, 'in', 'Get project name from overridden baseDir');
+			assert.equal(project.url, '/p1/about/');
+
+			project = fileMap['p2/img/smiley.png'];
+			assert.equal(project.prefix, prefix);
+			assert.equal(project.name, 'in', 'Get project name from parent overridden baseDir');
+			assert.equal(project.url, '/p2/img/smiley.png');
 
 			done();
 		});
